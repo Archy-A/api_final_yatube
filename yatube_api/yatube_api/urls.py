2fa5c970d74de14e87a32c26e58fa384132fa15f
Artur Avtukhov <archy6754267@gmail.com>
2022-04-20 01:07:15 +0200

    final_api_v007

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -4,13 +4,10 @@ from django.views.generic import TemplateView
 from django.contrib import admin
 from django.urls import include, path
 
-namespace = 'api'
+namespace = "api"
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path(
-        'redoc/',
-        TemplateView.as_view(template_name='redoc.html'),
-        name='redoc'
-    ),
+    path("admin/", admin.site.urls),
+    path("api/", include("api.urls")),
+    path("redoc/", TemplateView.as_view(template_name="redoc.html"), name="redoc"),
+]
