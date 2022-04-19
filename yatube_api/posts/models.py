190e1af92f4a341b4ac2341c47bca482239b7c18
Artur Avtukhov <archy6754267@gmail.com>
2022-04-20 00:45:28 +0200

    final_api_v003

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -4,13 +4,14 @@ from django.db import models
 User = get_user_model()
 
 
-class Group(models.Model): 
-    title = models.CharField(max_length=200) 
-    slug = models.SlugField(unique=True) 
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
     description = models.TextField()
 
-    def __str__(self): 
-        return self.title 
+    def __str__(self):
+        return self.title
+
 
 class Post(models.Model):
     text = models.TextField()
@@ -20,7 +21,7 @@ class Post(models.Model):
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True)
     group = models.ForeignKey(Group, on_delete=models.CASCADE,
-     related_name="posts", blank=True, null=True) 
+        related_name="posts", blank=True, null=True)
 
     def __str__(self):
         return self.text
